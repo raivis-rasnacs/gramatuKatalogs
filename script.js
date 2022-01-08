3601d5cb069fa65a73349b2a320ad716262de918
SkolotajsRaivis <71266644+raivis-rasnacs@users.noreply.github.com>
2022-01-08 11:22:12 +0000

    Added comments

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-var saraksts = document.getElementById("saraksts");
+var saraksts = document.getElementById("saraksts"); // Nodefinē tabulas elementu
 
-function pievienot() {
-  var nosaukums = document.getElementById("nosaukums").value;
-  var autors = document.getElementById("autors").value;
+function pievienot() { // Funkcija, kas pievieno ierakstu tabulā
+  var nosaukums = document.getElementById("nosaukums").value; // Izgūst nosaukumu no ievades lauka 'nosaukuma'
+  var autors = document.getElementById("autors").value; // Izgūst autoru no ievades lauka 'autors'
   
-  var jaunaGramata = document.createElement("tr");
-  var nosaukumaSuna = document.createElement("td");
-  nosaukumaSuna.innerHTML = nosaukums;
-  var autoraSuna = document.createElement("td");
-  autoraSuna.innerHTML = autors;
-  var pogasSuna = document.createElement("td");
-  var poga = document.createElement("button");
-  poga.setAttribute("onclick", "dzestGramatu(this.parentElement.parentElement)");
-  poga.innerHTML = "X";
-  pogasSuna.appendChild(poga);
-  jaunaGramata.appendChild(nosaukumaSuna);
-  jaunaGramata.appendChild(autoraSuna);
-  jaunaGramata.appendChild(pogasSuna);
-  jaunaGramata.id = "jaunaRinda";
-  saraksts.appendChild(jaunaGramata);
+  var jaunaGramata = document.createElement("tr"); // Izveido jaunu rindu jaunai grāmatati
+  var nosaukumaSuna = document.createElement("td"); // Izveido jaunu šūnu
+  nosaukumaSuna.innerHTML = nosaukums; // Šūnā iestata nosaukumu no mainīgā 'nosaukums'
+  var autoraSuna = document.createElement("td"); // Izveido jaunu šūnu
+  autoraSuna.innerHTML = autors; // Šūnā iestata autoru no mainīgā 'autors'
+  var pogasSuna = document.createElement("td"); // Izveido jaunu šūnu pogai
+  var poga = document.createElement("button"); // Izveido pogu
+  poga.setAttribute("onclick", "dzestGramatu(this.parentElement.parentElement)"); // Nodefinē funkcijas izsaukumu pogai
+  poga.innerHTML = "X"; // Pogai iestata tekstu 'X'
+  pogasSuna.appendChild(poga); // Pogu ievieto šūnā
+  jaunaGramata.appendChild(nosaukumaSuna); // Rindai pievieno nosaukuma šūnu
+  jaunaGramata.appendChild(autoraSuna); // Rindai pievieno autora šūnu
+  jaunaGramata.appendChild(pogasSuna); // Rindai pievieno pogas šūnu
+  saraksts.appendChild(jaunaGramata); // Rindu pievieno tabulai
 }
 
-function dzestGramatu(rinda) {
-  saraksts.removeChild(rinda);
+function dzestGramatu(rinda) { // Funkcijas, kas saņem parametru - attiecīgo tabulas rindu
+  saraksts.removeChild(rinda); // Izdzēš no tabulas attiecīgo rindu
 }
